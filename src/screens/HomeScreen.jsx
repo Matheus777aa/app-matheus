cf01f15437b58f123fc1c8d49aa56982f11b6ebc
Your Name <you@example.com>
2024-03-14 16:13:48 -0300

    ogggg

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import { useNavigation } from "@react-navigation/native";
+import { Button, Text, View } from "react-native";
+import { Image } from "expo-image";
+ 
+export default function HomeScreen({}) {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Text>Inicial</Text>
+      <Text>Bem vindo</Text>
+      <Image
+        source={require('../../assets/icon.png')}
+        style={{ width: 200, height: 200 }}
+      />
+      <Button
+        onPress={() => navigation.navigate("SobreScreen")}
+        title="Ir para sobre"
+      />
+    </View>
+  );
+}
